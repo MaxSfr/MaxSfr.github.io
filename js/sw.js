5a60ef82acd6abc2fa9c24a4adf70f45bbf29027
Maxime SERREE <maxserree@free.fr>
2019-11-19 11:15:31 +0100

    sw test 3

--- js/sw.js
+++ js/sw.js
@@ -14,7 +14,7 @@ var appShellFiles = [
     '/images/logo.png',
 ]
 
-elf.addEventListener('install', (e) => {
+self.addEventListener('install', (e) => {
   console.log('[Service Worker] Install');
   e.waitUntil(
     caches.open(cacheName).then((cache) => {
